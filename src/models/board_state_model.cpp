f709df9a2ba07766e3c945fe12c997bd02ade9e5
mostafa elspagh <mostafaelspagh0@gmail.com>
2020-05-03 19:04:44 +0200

    replace old define metnod with chess_peice enum

--- src/models/board_state_model.cpp
+++ src/models/board_state_model.cpp
@@ -1,53 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#include "chess_piece.cpp"
 using namespace std;
 #define A1 21
 #define H1 28
 #define A8 91
 #define H8 98
-#define p 1
-#define n 2
-#define k 3
-#define b 5
-#define r 6
-#define q 7
 
-int get_index(string s)
-{
-    return((s[1] - '0' +1) * 10) + s[0] - 'A' + 1;
-}
+int get_index(string s) { return ((s[1] - '0' + 1) * 10) + s[0] - 'A' + 1; }
 
-struct board_state 
-{
-    int board[120];
+struct board_state {
+  Chess_piece board[120];
 };
-int* get_initialboard()
-{
-    int b[120];
-    string s;
-    for (int i = 31; i <= 38; i++)
-    {
-        b[i] = 1;
-        
-    }
-    for (int i = 81; i <= 88; i++)
-    {
-        b[i] = -1;
+board_state get_initialboard() {
+  board_state r;
+  memset(r.board, 0, sizeof(r.board));
+  Chess_piece* b = r.board;
 
-    }
-    b[21] =b[28] = r;
-    b[22] = b[27] = n;
-    b[23] = b[26] = b;
-    b[24] = q;
-    b[25] = k;
+  string s;
+  for (int i = 31; i <= 38; i++) {
+    b[i] = wPawn;
+  }
+  for (int i = 81; i <= 88; i++) {
+    b[i] = bPawn;
+  }
+  b[21] = b[28] = wRock;
+  b[22] = b[27] = wKnight;
+  b[23] = b[26] = wBishop;
+  b[24] = wQueen;
+  b[25] = wKing;
 
-    b[91] = b[98] = -r;
-    b[92] = b[97] = -n;
-    b[93] = b[96] = -b;
-    b[94] = q;
-    b[95] = k;
-    return b;
+  b[91] = b[98] = bRock;
+  b[92] = b[97] = bKnight;
+  b[93] = b[96] = bBishop;
+  b[94] = bQueen;
+  b[95] = bKing;
+  return r;
 }
-int main()
-{
-
-}
+int main() {}
---
+++ src/models/chess_piece.cpp
@@ -0,0 +1,16 @@
+enum Chess_piece {
+  wPawn = 1,
+  wKnight = 2,
+  wKing = 3,
+  wBishop = 5,
+  wRock = 6,
+  wQueen = 7,
+  bPawn = -1,
+  bKnight = -2,
+  bKing = -3,
+  bBishop = -5,
+  bRock = -6,
+  bQueen = -7,
+  blank = 0
+};
+
